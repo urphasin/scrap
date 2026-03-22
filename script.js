708bd570641ab2148ee8c1f1794049d1eb8ee958
Otito Amuga <urphasin@gmail.com>
2026-03-22 14:34:46 -0600

    m m

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-let nr1 = 2;
-let nr2 = "3";
-
-
-
-let arr = [1, 2, 3, 4];
-
-
-
-console.dir(window)
-
-console.log("\n\n")
-
-console.log(document.querySelectorAll(".hello"));
-
 function videoStuff() {
   const video = document.querySelector("video");
   window.addEventListener("scroll", () => {
@@ -22,14 +7,35 @@ function videoStuff() {
 }
 videoStuff()
 
-
 function setVideoSrc() {
   const video = document.getElementById("bg-video");
 
   const width = window.innerWidth;
 
-  if (width < 768) {video.src = "assets/shorts/video-720.mp4";}
+  if (width < 768) { video.src = "assets/shorts/video-720.mp4"; }
   else video.src = "assets/shorts/video-1080.mp4";
 }
 
-setVideoSrc()
+
+
+function main() {
+  let nr1 = 2;
+  let nr2 = "3";
+
+
+
+  let arr = [1, 2, 3, 4];
+
+
+
+  console.dir(window)
+
+  console.log("\n\n")
+
+  console.log(document.querySelectorAll(".hello"));
+  
+  setVideoSrc()
+
+
+}
+main()
